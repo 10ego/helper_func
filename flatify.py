0abc34c3afe180e7306cdadbd4381c0b76350813
Ted Kim <ted.kim03@gmail.com>
2018-12-07 00:43:17 -0500

    Update flatify.py

--- flatify.py
+++ flatify.py
@@ -1,19 +1,18 @@
-#Input any nested json object to return it as a single value to add as a new row in your dataframe
+#Input any nested json object to return it as a single dict to add as a new row in your dataframe
 
 def flatify(json):
     row={}
-    
     def engine(data, header=''):
-        if type(data) is dict:
-            for obj in data:
-                engine(obj, header+obj+'.')
-        elif type(data) is list:
+        if type(data) is list:
             c=0
-            for elem in data:
-                engine(elem, header+str(c)+'..')
+            for obj in data:
+                engine(obj, '{}[{}]_'.format(header,str(c)))
                 c+=1
+        elif type(data) is dict:
+            for k,v in data.items():
+                engine(v, header+'.'+k)
+        
         else:
             row[header]=data
-            
     engine(json)
     return row
